4c80ac8ab2dc4c80ecfab4b8c6f873a8795c0aec
bensonhuang2020 <bensonhuang2020@gmail.com>
2026-09-02 16:30:43 -0700

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        res.append([])
+        if not nums:
+            return res
+
+        def build_subset(curr, index):
+            nonlocal res
+            """
+            # for each index, we create a new list based on our current list. starts with [], so it's just empty lists. we're using curr as the base case, then we add a list number, which appends to the current list. result takes this as a result, then recursively call the list with this as a starting point. this does the backtracking.
+            for i in range(index, len(nums)):
+                new_list = curr[:] + [nums[i]]
+                res.append(new_list)
+                build_subset(new_list, i + 1)
+            """
+            # backtracking style
+            for i in range(index, len(nums)):
+                curr.append(nums[i])
+                res.append(curr[:])
+                build_subset(curr, i + 1)
+                curr.pop()
+        build_subset([], 0)
+        return res
