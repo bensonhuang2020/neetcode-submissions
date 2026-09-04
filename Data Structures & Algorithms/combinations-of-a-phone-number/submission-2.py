18dd6040873cf8f7b8e06fa76e71b6f1c8610fd9
bensonhuang2020 <bensonhuang2020@gmail.com>
2026-09-03 18:09:38 -0700

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # set this up with a dictionary that maps numbers to the potential chars.
+        # digit len tells us how far to go, curr_word is just the backtracking mechanism. base case is that if we hit greater than digit len, we're OOB. then for each number, we go through all of the possible characters, append, then explore and backtrack.
+
+        phone_dict = {"2" : "abc", "3" : "def", "4" : "ghi", "5": "jkl", "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
+        digit_len = len(digits) - 1
+        res = []
+        curr_word = []
+        
+        def dfs(i):
+            if i > digit_len:
+                return
+            for x in phone_dict[digits[i]]:
+                curr_word.append(x)
+                if i == digit_len:
+                    res.append("".join(curr_word))
+                dfs(i + 1)
+                curr_word.pop()
+        dfs(0)
+        return res
